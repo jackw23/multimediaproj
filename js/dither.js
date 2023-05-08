dd46df65721831dbcfa642a0c60274b8da1953b9
chentiffany <c.tiffany@wustl.edu>
2023-05-09 02:04:08 +0800

    we did something ?

--- js/dither.js
+++ js/dither.js
@@ -5,57 +5,100 @@
    * Apply ordered dithering to the input data
    */
   imageproc.dither = function(inputData, outputData, matrixType) {
-      console.log("Applying error dithering!!");
 
-      var error = 0;
-      var threshold = 128;
-      var result;
-      
-      console.log("threshold: " + threshold);
+    switch(matrixType) {
+      case "error":
+        console.log("error diffusion");
+        var error = 0;
+        var threshold = 128;
+        var result;
 
-      for (var y = 0; y < inputData.height; y++) {
-          for (var x = 0; x < inputData.width; x++) {
-              var pixel = imageproc.getPixel(inputData, x, y);
-              var grayscale = ((pixel.r + pixel.g + pixel.b) / 3) + error;
+        var errorBuffer = new Array(inputData.width);
+        for (var x = 0; x < inputData.width; x++) {
+          errorBuffer[x] = new Array(inputData.height).fill(0);
+        }
+          for (var y = 0; y < inputData.height; y++) {
+              for (var x = 0; x < inputData.width; x++) {
+                  var pixel = imageproc.getPixel(inputData, x, y);
+                  var grayscale = (pixel.r + pixel.g + pixel.b) / 3;
+                  grayscale = grayscale - errorBuffer[x][y];
+                  var result = grayscale < threshold ? 0 : 255;
 
-              var result = grayscale < 200 ? 0 : 255;
-              error = grayscale - result;
+                  var i = (x + y * outputData.width) * (4);
+                  outputData.data[i] = result;
+                  outputData.data[i + 1] = result;
+                  outputData.data[i + 2] = result;
 
-              var i = (x + y * outputData.width) * (4);
+                  error = grayscale - result;
+                  
+                  if(x < inputData.width - 1){
+                      errorBuffer[x + 1][y] -= error * (7.0/16);
+                  }
 
-              outputData.data[i] = result;
-              outputData.data[i + 1] = result;
-              outputData.data[i + 2] = result;
-               
-              if(x < inputData.width - 1){
-                  var ri = ((x+1) + y * outputData.width) * (4);
-                  outputData.data[ri]     += (error*(7.0/16));
-                  outputData.data[ri + 1] += (error*(7.0/16));
-                  outputData.data[ri + 2] += (error*(7.0/16));
-              }
+                  if(x > 0 && y < inputData.height - 1){
+                      errorBuffer[x-1][y+1] -= error * (3.0/16);
+                  }
+                  
+                  if(y < inputData.height - 1){
+                      errorBuffer[x][y+1] -= error * (5.0/16);
+                  }
 
-              if(x > 0 && y < inputData.height - 1){
-                  var ldi = ((x-1) + (y+1) * outputData.width) * (4);
-                  outputData.data[ldi]     += (error*(3.0/16));
-                  outputData.data[ldi + 1] += (error*(3.0/16));
-                  outputData.data[ldi + 2] += (error*(3.0/16));
-              }
-              
-              if(y < inputData.height - 1){
-                  var di = (x + (y+1) * outputData.width) * (4);
-                  outputData.data[di]     += (error*(5.0/16));
-                  outputData.data[di + 1] += (error*(5.0/16));
-                  outputData.data[di + 2] += (error*(5.0/16));
-              }
-
-              if(x < inputData.width - 1 && y < inputData.height - 1){
-                  var rdi = ((x+1) + (y+1) * outputData.width) * (4);
-                  outputData.data[rdi]     += (error*(1.0/16));
-                  outputData.data[rdi + 1] += (error*(1.0/16));
-                  outputData.data[rdi + 2] += (error*(1.0/16));
+                  if(x < inputData.width - 1 && y < inputData.height - 1){
+                      errorBuffer[x+1][y+1] -= error * (1.0/16);
+                  }
               }
           }
-      }
+        break;
+      case "poster":
+        console.log("to posterization");
+        // var threshold = 128;
+        var result;
+
+        var levels = 8; 
+        var step = 255 / (levels - 1);
+
+        var errorBuffer = new Array(inputData.width);
+        for (var x = 0; x < inputData.width; x++) {
+          errorBuffer[x] = new Array(inputData.height).fill(0);
+        }
+
+          // console.log("threshold: " + threshold);
+          for (var y = 0; y < inputData.height; y++) {
+              for (var x = 0; x < inputData.width; x++) {
+                  var pixel = imageproc.getPixel(inputData, x, y);
+                  var grayscale = (pixel.r + pixel.g + pixel.b) / 3;
+                  grayscale = grayscale - errorBuffer[x][y];
+
+                  var level = Math.round(grayscale / step) * step;
+                  var result = level > 255 ? 255 : level < 0 ? 0 : level;
+
+                  var i = (x + y * outputData.width) * (4);
+                  outputData.data[i] = result;
+                  outputData.data[i + 1] = result;
+                  outputData.data[i + 2] = result;
+
+                  var error = grayscale - result;
+                  
+                  if(x < inputData.width - 1){
+                      errorBuffer[x + 1][y] -= error * (7.0/16);
+                  }
+
+                  if(x > 0 && y < inputData.height - 1){
+                      errorBuffer[x-1][y+1] -= error * (3.0/16);
+                  }
+                  
+                  if(y < inputData.height - 1){
+                      errorBuffer[x][y+1] -= error * (5.0/16);
+                  }
+
+                  if(x < inputData.width - 1 && y < inputData.height - 1){
+                      errorBuffer[x+1][y+1] -= error * (1.0/16);
+                  }
+              }
+          }
+        break;
+    }
+
   }
 
 }(window.imageproc = window.imageproc || {}));
