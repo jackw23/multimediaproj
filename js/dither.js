1717456d80bbccf95779a90881110cf5d2af3f27
chentiffany <c.tiffany@wustl.edu>
2023-05-09 20:40:40 +0800

    everything

--- js/dither.js
+++ js/dither.js
@@ -5,36 +5,29 @@
    * Apply ordered dithering to the input data
    */
   imageproc.dither = function(inputData, outputData, matrixType, levels) {
-    console.log(levels);
 
     switch(matrixType) {
-      case "error":
-        console.log("error diffusion!")
+      case "poster":
+        console.log("poster!");
         var resultR;
         var resultG;
         var resultB;
-        var errorR = 0;
-        var errorG = 0;
-        var errorB = 0;
 
-        var redLevels = Math.pow(2, 2);
-        var greenLevels = Math.pow(2, 2);
-        var blueLevels = Math.pow(2, 2);
-      
-        var redMask = Math.floor(256 / redLevels) - 1;
-        var greenMask = Math.floor(256 / greenLevels) - 1;
-        var blueMask = Math.floor(256 / blueLevels) - 1;
+        console.log(levels);
+        var step = 255/ (levels - 1);
 
         var errorBuffer = new Array(inputData.width);
         for (var x = 0; x < inputData.width; x++) {
           errorBuffer[x] = new Array(inputData.height).fill([0,0,0]);
         }
+
           for (var y = 0; y < inputData.height; y++) {
               for (var x = 0; x < inputData.width; x++) {
                   var pixel = imageproc.getPixel(inputData, x, y);
+                  var grayscale = (pixel.r + pixel.g + pixel.b) / 3;
 
-                  var levelR = Math.floor(pixel.r / (256 / redLevels)) * redMask;
-                  levelR = levelR - errorBuffer[x][y][0];
+                  var grayscaleR = pixel.r - errorBuffer[x][y][0];
+                  var levelR = Math.round(grayscaleR/step) * step;
                   if (levelR > 255) {
                     resultR = 255;
                   } else if (levelR < 0) {
@@ -42,10 +35,9 @@
                   } else {
                     resultR = levelR;
                   }
-                  errorR = pixel.r - resultR;
 
-                  var levelG = Math.floor(pixel.g / (256 / greenLevels)) * greenMask;
-                  levelG = levelG - errorBuffer[x][y][1];
+                  var grayscaleG = pixel.g - errorBuffer[x][y][1];
+                  var levelG = Math.round(grayscaleG/step) * step;
                   if (levelG > 255) {
                     resultG = 255;
                   } else if (levelG < 0) {
@@ -53,10 +45,9 @@
                   } else {
                     resultG = levelG;
                   }
-                  errorG = pixel.g - resultG;
 
-                  var levelB = Math.floor(pixel.b / (256 / blueLevels)) * blueMask;
-                  levelB = levelB - errorBuffer[x][y][2];
+                  var grayscaleB = pixel.b - errorBuffer[x][y][2];
+                  var levelB = Math.round(grayscaleB/step) * step;
                   if (levelB > 255) {
                     resultB = 255;
                   } else if (levelB < 0) {
@@ -64,13 +55,33 @@
                   } else {
                     resultB = levelB;
                   }
-                  errorB = pixel.b - resultB;
 
-                  var i = (x + y * outputData.width) * (4);
-                  outputData.data[i] = resultR;
-                  outputData.data[i + 1] = resultG;
-                  outputData.data[i + 2] = resultB;
+                  var redLevels = Math.pow(2,2);
+                  var greenLevels = Math.pow(2,2);
+                  var blueLevels = Math.pow(2,2);
+
+                  var redMask = Math.floor(256/redLevels) - 1;
+                  var greenMask = Math.floor(256/greenLevels) - 1; 
+                  var blueMask = Math.floor(256/blueLevels) - 1;
+
+                  var rVal = Math.floor(inputData.data[i] / (256/redLevels)) * redMask;
+                  var gVal = Math.floor(inputData.data[i + 1] / (256/greenLevels)) * greenMask;
+                  var bVal = Math.floor(inputData.data[i + 2] / (256/blueLevels)) * blueMask;
+
+                  var i = (x + y * outputData.width) * (4); 
+                  outputData.data[i] = resultR & rVal;
+                  outputData.data[i + 1] = resultG & gVal;
+                  outputData.data[i + 2] = resultB & bVal;
                   outputData.data[i + 3] = 255;
+
+                  // var i = (x + y * outputData.width) * (4);
+                  // outputData.data[i] = result;
+                  // outputData.data[i + 1] = result;
+                  // outputData.data[i + 2] = result;
+
+                  var errorR = grayscaleR - resultR;
+                  var errorG = grayscaleG - resultG;
+                  var errorB = grayscaleB - resultB;
                   
                   if(x < inputData.width - 1){
                       errorBuffer[x+1][y][0] -= errorR * (7.0/16);
@@ -98,10 +109,10 @@
               }
           }
         break;
-      case "poster":
-        console.log("to posterization");
-        var result;
+      case "error":
+        console.log("dithering!");
         console.log(levels);
+        var result;
         var step = 255/ (levels - 1);
 
         var errorBuffer = new Array(inputData.width);
